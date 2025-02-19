405a2d3f0e959ce4a4b7f44c36fc3993c317793d
OTWhite04 <owen.t.white2005@gmail.com>
2025-02-19 14:24:44 -0400

    Adding scripts, player and title screen

---
+++ Assets/Scripts/UIManagerScript.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIManagerScript : MonoBehaviour
+{
+    //int for the score and reference for the TextMeshPro.
+    public int score;
+
+    public TextMeshProUGUI scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    //Method for increasing the score by 20 after pushing the button.
+    public void ScoreUp()
+    {
+        score += 20;
+        UpdateScoreUI();
+    }
+
+    //Method for decreasing the score by 20 after pushing a button.
+    public void ScoreDown()
+    {
+        score -= 20;
+        UpdateScoreUI();
+    }
+
+    //Resets the score after pushing a button.
+    public void ScoreReset()
+    {
+        score = 0;
+        UpdateScoreUI();
+    }
+
+    //Method for updating the score UI.
+    public void UpdateScoreUI()
+    {
+        scoreText.text = score.ToString();
+    }
+
+}
+
