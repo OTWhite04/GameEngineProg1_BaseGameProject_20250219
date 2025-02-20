782f2a67052b06b30765f1967c4e2c9556457679
OTWhite04 <owen.t.white2005@gmail.com>
2025-02-20 15:18:24 -0400

    Removed Score code

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,43 +5,7 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    //int for the score and reference for the TextMeshPro.
-    public int score;
-
-    public TextMeshProUGUI scoreText;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        score = 0;
-    }
-
-    //Method for increasing the score by 20 after pushing the button.
-    public void ScoreUp()
-    {
-        score += 20;
-        UpdateScoreUI();
-    }
-
-    //Method for decreasing the score by 20 after pushing a button.
-    public void ScoreDown()
-    {
-        score -= 20;
-        UpdateScoreUI();
-    }
-
-    //Resets the score after pushing a button.
-    public void ScoreReset()
-    {
-        score = 0;
-        UpdateScoreUI();
-    }
-
-    //Method for updating the score UI.
-    public void UpdateScoreUI()
-    {
-        scoreText.text = score.ToString();
-    }
+    
 
     public GameObject mainMenu;
     public GameObject gamePlayUI;
